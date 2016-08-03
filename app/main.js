50c36a2ca9916e24fb627ff1110f4046ef3014ee
peter.mezei <peter.mezei@slamby.com>
2016-08-03 09:26:14 +0200

    ie 9 support removed. Bye bye.

--- app/main.js
+++ app/main.js
@@ -5,10 +5,8 @@ var app_component_1 = require('./app.component');
 // Our main routes
 var app_routes_1 = require('./app.routes');
 var core_1 = require('@angular/core');
-var common_1 = require('@angular/common');
 core_1.enableProdMode();
 platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent, [
-    app_routes_1.APP_ROUTER_PROVIDERS,
-    { provide: common_1.LocationStrategy, useClass: common_1.HashLocationStrategy }
+    app_routes_1.APP_ROUTER_PROVIDERS
 ]).catch(function (err) { return console.error(err); });
 //# sourceMappingURL=main.js.map
--- app/main.ts
+++ app/main.ts
@@ -8,12 +8,8 @@ import { APP_ROUTER_PROVIDERS } from './app.routes';
 
 import {enableProdMode} from '@angular/core';
 
-import { LocationStrategy,
-         HashLocationStrategy } from '@angular/common';
-
 enableProdMode();
 
 bootstrap(AppComponent,[
-  APP_ROUTER_PROVIDERS,
-  { provide: LocationStrategy, useClass: HashLocationStrategy }
+  APP_ROUTER_PROVIDERS
 ]).catch((err: any) => console.error(err))
