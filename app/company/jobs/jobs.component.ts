78d031eb5c2d091069414f48f942706a3f0116aa
peter.mezei <peter.mezei@slamby.com>
2016-08-16 14:41:03 +0200

    company/jobs page improvements added.

--- app/company/jobs/jobs.component.ts
+++ app/company/jobs/jobs.component.ts
@@ -18,6 +18,63 @@ export class JobsComponent implements OnInit {
                 private _jobsService: JobsService){
     }
 
+    team = [
+        {
+            "name":"Peter Mezei",
+            "image":"peter-mezei",
+            "position":"CEO",
+            "email":"peter.mezei@slamby.com"
+        },
+        {
+            "name":"Attila Laszlo",
+            "image":"attila-laszlo",
+            "position":"CTO",
+            "email":"attila.laszlo@slamby.com"
+        },
+        {
+            "name":"Bela Soós",
+            "image":"bela-soos",
+            "position":"Developer",
+            "email":"bela.soos@slamby.com"
+        },
+        {
+            "name":"Zsolt Illés",
+            "image":"zsolt-illes",
+            "position":"Developer",
+            "email":"zsolt.illes@slamby.com"
+        },
+        {
+            "name":"Zsolt Rábai",
+            "image":"zsolt-rabai",
+            "position":"Mathematician",
+            "email":"zsolt.rabai@slamby.com"
+        },
+        {
+            "name":"Agnes Nagy",
+            "image":"agnes-nagy",
+            "position":"Mathematician",
+            "email":"agnes.nagy@slamby.com"
+        },
+        {
+            "name":"Örsel Sani",
+            "image":"orsel-sani",
+            "position":"Head of Community",
+            "email":"orsel.sani@slamby.com"
+        },
+        {
+            "name":"Judit Gál",
+            "image":"judit-gal",
+            "position":"Project Manager",
+            "email":"judit.gal@slamby.com"
+        },
+        {
+            "name":"Rita Papp",
+            "image":"rita-papp",
+            "position":"Product Development Advisor",
+            "email":"rita.papp@slamby.com"
+        }
+    ]
+
     ngOnInit(): void {
         this._jobsService.getJobs()
                 .subscribe(
