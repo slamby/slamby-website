76bdf6ae28bcc1927f6fd686a3b8a30c2ca77dc8
peter.mezei <peter.mezei@slamby.com>
2016-09-07 09:12:53 +0200

    Title added.

--- app/tau/tau.component.ts
+++ app/tau/tau.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, AfterContentInit } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
+import {Title} from "@angular/platform-browser";
 
 @Component({
     templateUrl: '/app/tau/tau.component.html',
     styleUrls: ['app/tau/tau.component.css'],
-    directives: [ROUTER_DIRECTIVES]
+    directives: [ROUTER_DIRECTIVES],
+    providers: [Title]
 })
-export class TauComponent {
-    public pageTitle: string = 'Welcome';
+export class TauComponent implements AfterContentInit {
+    public constructor(private titleService: Title ) { }
+
+    public ngAfterContentInit(){
+        this.titleService.setTitle( "TAU as a productivity tool for your Slamby Server - Slamby" );
+    }
 }
