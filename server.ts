b60f15da5688cf8fde1124aab59a0b6a4cb10a52
Ubuntu <slamby@slambysites.sc30zygmdmfuricjgcotethnoa.fx.internal.cloudapp.net>
2016-12-01 18:35:35 +0000

    zonejs version + app name mod

--- server.ts
+++ server.ts
@@ -28,5 +28,5 @@ app.listen(port, '0.0.0.0', (err) => {
     if (err) {
         console.log(err);
     }
-    console.info('Server is running on loclahost at port: %s', port);
+    console.info('Server is running on localhost at port: %s', port);
 });
