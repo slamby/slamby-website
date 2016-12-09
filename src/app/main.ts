f29e28b2ca80031f3f8599b4a2e79a871b32ffe3
Peter Mezei <peter.mezei@slamby.com>
2016-12-09 11:40:52 +0100

    updates, release

--- src/app/main.ts
+++ src/app/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 require('!style!css!sass!../assets/_site.scss');
 require('!style!css!font-awesome/css/font-awesome.min.css');
-//require('./google-analytics.js');
+require('./google-analytics.js');
 
 enableProdMode();
 
