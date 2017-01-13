5edcc2df9adf00cc734bb46baf01d27798258ea2
Peter Mezei <peter.mezei@slamby.com>
2017-01-13 13:49:36 +0100

    legal fixes done.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -38,41 +38,49 @@ import { SupportComponent } from './support/support.component';
       },
       {
         path: 'home',
-        component: HomeComponent
+        component: HomeComponent,
+        pathMatch: 'full'
       },
       {
         path: 'insight',
         component: InsightComponent,
+        pathMatch: 'full',
         data: {meta:{title:"Insight"}}
       },
       {
         path: 'pricing', 
         component: PricingComponent,
+        pathMatch: 'full',
         data: {meta:{title:"Pricing",description:"Slamby pricing, on-premise, SaaS, core-based, open-source."}}
       },
       {
         path: '404',
         component: NotFoundPageComponent,
+        pathMatch: 'full',
         data: {meta:{title:"Page not found",description:"The requested page is not found."}}
       },
       {
         path: 'thank-you',
         component: ThankYouComponent,
+        pathMatch: 'full',
         data: {meta:{title:"Thank You" }}
       },
       {
         path: 'getting-started',
         component: GettingStartedComponent,
+        pathMatch: 'full',
         data: {meta:{title:"Getting started",description:"Quick tutorial about Slamby install steps, pricing, licenses, support, SDKs, related softwares and Slamby Insight."}}
       },
       {
         path: 'partners',
         component: PartnersComponent,
+        pathMatch: 'full',
         data: {meta:{title:"Partners",description:"Slamby partners who helped a lot to us."}}
       },
       {
         path: 'company',
         component: CompanyComponent,
+        pathMatch: 'full',
         data: {meta:{title:"Company",description:"We love what we do, if you need more information about Slamby, just contact us anytime at hello@slamby.com"}}
       },
       {
@@ -84,31 +92,37 @@ import { SupportComponent } from './support/support.component';
       {
         path: 'legal/privacy-policy',
         component: PrivacyPolicyComponent,
+        pathMatch: 'full',
         data: {meta:{title:"Privacy policy"}}
       },
       {
         path: 'legal/commercial-license',
         component: CommercialLicenseComponent,
+        pathMatch: 'full',
         data: {meta:{title:"Commercial license"}}
       },
       {
         path: 'legal/commercial-license-oem',
         component: CommercialLicenseOemComponent,
+        pathMatch: 'full',
         data: {meta:{title:"Commercial license for OEMs"}}
       },
       {
         path: 'legal/gnu-agpl-license',
         component: GnuAgplLicenseComponent,
+        pathMatch: 'full',
         data: {meta:{title:"GNU AGPL V3 license"}}
       },
       {
         path: 'legal/open-source-license',
         component: OpenSourceLicenseComponent,
+        pathMatch: 'full',
         data: {meta:{title:"Open-source license"}}
       },
       {
         path: 'support',
         component: SupportComponent,
+        pathMatch: 'full',
         data: {meta:{title:"Support"}}
       },
       {
