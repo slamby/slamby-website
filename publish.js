267feefee403523ef72b02deae12daa1fb603ef8
Peter Mezei <peter.mezei@slamby.com>
2017-01-15 21:08:44 +0100

    Release commit version: 2.0.1

---
+++ publish.js
@@ -0,0 +1,12 @@
+var exec = require('child_process').exec;
+var version = require('./package.json')['version'];
+
+// Compile aot prod version with CDN base url.
+// exec('ng build --prod --aot --base-href https://cdn.rawgit.com/slamby/slamby-website/' + version + '/', {maxBuffer: 1024 * 500}, function(error, stdout, stderr) {
+//     console.log(error);
+// });
+
+require('simple-git')()
+    .add('./*')
+    .commit("Release commit version: " + version)
+    .push()
