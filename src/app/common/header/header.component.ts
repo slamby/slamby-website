2e965f180ae857b2b4ec31a32c60aa1065734fdb
Peter Mezei <peter.mezei@slamby.com>
2017-01-19 14:17:32 +0100

    re

--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -11,7 +11,7 @@ export class HeaderComponent {
     @Input() theme: String = 'theme-default';
     menuItems = [
         new MenuItem('Slamby', '/home', 'logo', false),
-        // new MenuItem('Insight', '/insight', '', false),
+        new MenuItem('Insight', '/insight', '', false),
         new MenuItem('Pricing', '/pricing', '', false),
         new MenuItem('Support', '/support', '', false),
         // new MenuItem('Blog', 'http://blog.slamby.com', '', true),
