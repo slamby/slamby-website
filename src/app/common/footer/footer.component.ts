1092b19a99e77d5b89e90899dca8265b2038d84e
Peter Mezei <peter.mezei@slamby.com>
2017-01-19 15:27:37 +0100

    New footer

--- src/app/common/footer/footer.component.ts
+++ src/app/common/footer/footer.component.ts
@@ -8,4 +8,12 @@ import { Component } from '@angular/core';
 
 export class FooterComponent {
     year = new Date().getFullYear();
+
+    socialLinks = [
+        {'name': 'Facebook', 'link': 'https://www.facebook.com/slambynews', 'class': 'fa-facebook-f'},
+        {'name': 'Twitter', 'link': 'https://twitter.com/slambynews', 'class': 'fa-twitter'},
+        {'name': 'LinkedIn', 'link': 'https://www.linkedin.com/company/slamby', 'class': 'fa-linkedin'},
+        {'name': 'Instagram', 'link': 'https://www.instagram.com/slambynews/', 'class': 'fa-instagram'},
+        {'name': 'YouTube', 'link': 'https://www.youtube.com/channel/UCQ6UfPZshXDIXT0zi3xRyHQ', 'class': 'fa-youtube-play'}
+    ]
 }
