00f6e0cbf07d15dc539cc9ddf49e3e7cf7189b0e
Peter Mezei <peter.mezei@slamby.com>
2017-01-20 15:58:22 +0100

    Release commit version: 2.2.11

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,8 +1,48 @@
 import { Component } from '@angular/core';
+import { LeadService } from './lead.service';
+import {Observable} from 'rxjs/Rx';
 
 @Component({
     templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss']
+    styleUrls: ['./home.component.scss'],
+    providers: [LeadService]
 })
 
-export class HomeComponent {}
+export class HomeComponent {
+    constructor(private leadService: LeadService) {}
+
+    // tslint:disable-next-line:member-ordering
+    leadDetails = {
+        'FIRST_NAME': '',
+        'LAST_NAME': '',
+        'ORGANIZATION_NAME': '',
+        'EMAIL_ADDRESS': ''
+    };
+
+    isValid: boolean = true;
+
+    validateForm() {
+        this.isValid = true; // set it back for new checking;
+        for (let i = 0; i < Object.keys(this.leadDetails).length; i++){
+            if (this.leadDetails[Object.keys(this.leadDetails)[i]].length === 0) {
+                this.isValid = false;
+            }
+        }
+    };
+
+    // tslint:disable-next-line:member-ordering
+    leadCreatedIsSuccess: boolean = false;
+
+    createLead() {
+        this.validateForm();
+        if (this.isValid) {
+            this.leadService.createLead(this.leadDetails).subscribe(
+                res => {
+                    this.leadCreatedIsSuccess = true;
+                }, err => {
+                    console.log(err);
+                }
+            );
+        }
+    }
+}
--- src/app/home/lead.service.ts
+++ src/app/home/lead.service.ts
@@ -8,16 +8,23 @@ import 'rxjs/add/operator/catch';
 @Injectable()
 
 export class LeadService {
-    constructor (private http: Http) {}
+    constructor (
+        private http: Http,
+    ) {}
 
-    createLead(): Observable<any> {
+    createLead(
+        leadDetails = {
+            'FIRST_NAME': '',
+            'LAST_NAME': '',
+            'EMAIL_ADDRESS': '',
+            'ORGANIZATION_NAME': ''
+        }
+    ): Observable<any> {
         const body = {
-            'FIRST_NAME': 'Peter',
-            'LAST_NAME': 'Mezei',
-            'ORGANIZATION_NAME': 'Slamby',
-            'MOBILE_PHONE_NUMBER': '+36304375490',
-            'EMAIL_ADDRESS': 'peter.mezei@slamby.com',
-            'WEBSITE_URL': 'www.slamby.com',
+            'FIRST_NAME': leadDetails.FIRST_NAME,
+            'LAST_NAME': leadDetails.LAST_NAME,
+            'ORGANIZATION_NAME': leadDetails.ORGANIZATION_NAME,
+            'EMAIL_ADDRESS': leadDetails.EMAIL_ADDRESS,
             'VISIBLE_TO': 'EVERYONE',
             'LEAD_DESCRIPTION': 'source from website/ new campaign'
         }
@@ -27,7 +34,7 @@ export class LeadService {
         });
         const options = new RequestOptions({ headers: headers });
 
-        return this.http.post('http://localhost:3000/api/create-lead', bodyString, options)
+        return this.http.post('/api/create-lead', bodyString, options)
                          .map((res: Response) => res.json())
                          .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
     }
