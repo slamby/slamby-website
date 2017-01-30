56350ecec6337b936a046d82043452b7cac0acff
Peter Mezei <peter.mezei@slamby.com>
2017-01-30 10:54:59 +0100

    Release commit version: 2.4.1

--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -10,6 +10,8 @@ import { Router } from '@angular/router';
 
 export class HeaderComponent {
 
+    @Input() opacity: number;
+
     currentRouterUrl: string;
 
     constructor(private router: Router) {
